d8976ffe8bc201102207c172f151696e6d801a0a
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-01-09 23:38:45 +0600

    javaScript

---
+++ JavaScript/Conditional Statement/practice_2.js
@@ -0,0 +1,13 @@
+let size = "XL";
+
+if(size == "XL"){
+    console.log("Price is 250")
+}else if(size == "L"){
+    console.log("Price is 200");
+}else if(size == "M"){
+    console.log("Price is 150");
+} else if(size == "S"){
+    console.log("Price is 50");
+}else{
+    console.log("Invalid size");
+}
