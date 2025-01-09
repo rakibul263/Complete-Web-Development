d06542708db0b0cdbc70d23677166305c5ee9af3
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-01-09 12:58:58 +0600

    javaScript

---
+++ JavaScript/Conditional Statement/practice_1.js
@@ -0,0 +1,9 @@
+// create a trafic light system that show what to do based on the color of the light
+let light = "red";
+if(light === "red"){
+    console.log("Stop the car.");
+}else if(light === "yellow"){
+    console.log("Ready to move.");
+}else if(light === "green"){
+    console.log("Move the car.");
+}
