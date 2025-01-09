54dbd0c431b8f7f139627049140a4ee9df2f12a5
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-01-09 12:12:00 +0600

    JavaScript

---
+++ JavaScript/Template Literals/template_literals.js
@@ -0,0 +1,6 @@
+let pencilPrice = 10;
+let eraserPrice = 5;
+let total = pencilPrice + eraserPrice;
+// let output = "Total Price is: "+total+" Taka"
+let output = `Total Price is: ${total} Taka`;
+console.log(output);
---
+++ JavaScript/Variable/app.js
@@ -0,0 +1,7 @@
+console.log('Hello World!');
+console.log("Rakibul Hasan");
+
+let a = 10;
+let b = 20;
+let sum = a + b;
+console.log("Sum is: ",sum);
