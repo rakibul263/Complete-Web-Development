347c4bcc8058ec7a6310ac4dd45b9033d945a27a
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-01-13 16:18:32 +0600

    loop

---
+++ JavaScript/Loop/for_loop.js
@@ -0,0 +1,20 @@
+// print 1-5 
+for(let i = 1; i<=5;i++){
+    console.log(i);
+}
+
+console.log("**********");
+console.log("Print all odd number 1 to 15:");
+for(let i=1;i<=15;i++){
+    if(i%2==1){
+        console.log(i);
+    }
+}
+
+console.log("**********");
+console.log("Print all even number 2 to 10:");
+for(let i=2;i<=10;i++){
+    if(i%2==0){
+        console.log(i);
+    }
+}
