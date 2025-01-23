8e1d0c574b112f3578a2daad73dac2237ada0242
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-01-23 23:47:16 +0600

    js event onclick

--- JavaScript/Event/Example-01/app.js
+++ JavaScript/Event/Example-01/app.js
@@ -1,7 +1,13 @@
 let btn = document.querySelector('button');
 // console.dir(btn)
 
-btn.onclick = function(){
-    // console.log("Button Clicked");
-    alert("Button Clicked")
+// btn.onclick = function(){
+//     // console.log("Button Clicked");
+//     alert("Button Clicked")
+// }
+
+btn.onclick = showHello;
+
+function showHello(){
+    alert("Hello Shuvo");
 }
