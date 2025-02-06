b1394da81591feaa9539bb06c4f97b02e4a75e82
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-02-06 23:13:08 +0600

    js

--- JavaScript/Phitron JavaScript/array_distructuring.js
+++ JavaScript/Phitron JavaScript/array_distructuring.js
@@ -1,7 +1,21 @@
-const vehicles = ['toyota', 'marchetise', 'audi'];
+/* const vehicles = ['toyota', 'marchetise', 'audi'];
 
 const [car1, car2, car3] = vehicles;
 
 console.log(car1);
 console.log(car2);
-console.log(car3);
+console.log(car3); */
+
+function calculate(a, b){
+    const sum = a+b;
+    const sub = a-b;
+    const mul = a*b;
+    const div = a/b;
+
+    return [sum, sub, mul, div];
+}
+const [sum, sub, mul, div]  = calculate(15, 3);
+console.log(sum);
+console.log(sub);
+console.log(mul);
+console.log(div);
