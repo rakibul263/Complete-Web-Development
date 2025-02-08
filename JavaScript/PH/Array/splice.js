f54b6597324c383c211b92a3cd638296dc5281d8
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-02-09 00:31:49 +0600

    js

---
+++ JavaScript/PH/Array/splice.js
@@ -0,0 +1,5 @@
+const names = ["Shuvo", "Ragib", "Arman", "Bulbul", "Hasib", "Aysha", "Asha", "Mili"];
+const name = [];
+name  = names.splice(1,5);
+console.log(names);
+console.log("This is the splice value = ", name);
