428b9b1eb841078b526ee336e6c863db55abef79
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-02-09 00:22:32 +0600

    js

---
+++ JavaScript/PH/Array/isArray.js
@@ -0,0 +1,10 @@
+const students = ["Ragib", "Shuvo", "Rakibul"];
+
+const dept = ["cse", "eee"];
+
+const books = "hlw! this books name is perahin";
+
+console.log(Array.isArray(dept));
+console.log(Array.isArray(students));
+console.log(Array.isArray(books));
+
