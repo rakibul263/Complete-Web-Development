1aff7a9dd99ed9d68a23c4f401bae86bf5e0378e
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-02-08 23:45:22 +0600

    js

---
+++ JavaScript/PH/Array/add_remove.js
@@ -0,0 +1,25 @@
+const names = ["Shuvo", "Ragib", "Arman", "Bulbul", "Hasib", "Aysha", "Asha", "Mili"];
+// popoing array
+const poping = [];
+// add end of the array
+names.push("Maruf");
+console.log(names);
+names.push("Abir");
+console.log(names);
+
+
+// remove end of the array
+poping.push(names.pop());
+console.log(names);
+// console.log(poping);
+poping.push(names.pop());
+console.log(names);
+
+let ans = console.log("After Poping : ", poping);
+console.log("After poping : "+ poping);
+console.log(typeof(ans));
+
+// make it string 
+console.log("After adding the value last of the array = "+ names);
+// console it using datatype
+console.log("After adding the value last of the array = ", names);
