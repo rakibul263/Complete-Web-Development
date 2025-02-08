9ae2d619d2119efcd88ee436820246d7b45b6f15
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-02-08 23:33:39 +0600

    js

--- JavaScript/PH/Array/length.js
+++ JavaScript/PH/Array/length.js
@@ -1,2 +1,10 @@
 const person = ["Rohim", "Korim", "Josim", "Vola", "Moyra"];
 console.log(person.length);
+console.log(person[2]);
+console.log(person.indexOf("Korim"));
+
+
+
+const students = ["Shuvo", "Ragib", "Arman", "Bulbul", "Hasib", "Aysha", "Asha", "Mili"];
+console.log(students.indexOf("Asha"));
+console.log(students[6]);
