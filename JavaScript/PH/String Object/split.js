da6d36863b2753eb776e0149993a86024a50b340
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-02-09 21:41:42 +0600

    js

---
+++ JavaScript/PH/String Object/split.js
@@ -0,0 +1,15 @@
+const sentence = 'I am a Rakibul Hasan Shuvo, I am from Gaibandha';
+
+const newSentence = sentence.split(' ');
+
+console.log(sentence);
+console.log(newSentence);
+
+
+// type of the variable 
+console.log(typeof sentence);
+console.log(typeof newSentence);
+
+const nameSclice = newSentence.slice(3,6);
+console.log(nameSclice);
+console.log(newSentence[3]);
