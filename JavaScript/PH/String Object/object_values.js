766c6adabcce225fff9369106831cc6e1390a86c
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-02-09 23:37:35 +0600

    js

---
+++ JavaScript/PH/String Object/object_values.js
@@ -0,0 +1,10 @@
+const computer = {
+    brand : "Apple",
+    processor: "M3 Chip",
+    price : 200000,
+    display : "Retina Display"
+}
+
+const values = Object.values(computer);
+
+console.log(values);
