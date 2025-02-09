7ba105768ea599e1d3440ff0dc93f20b0b53c895
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-02-09 23:26:10 +0600

    js

---
+++ JavaScript/PH/String Object/object.js
@@ -0,0 +1,18 @@
+// premitive data type
+const name = "Rakibul Hasan";
+const age = 23;
+
+
+// non-premitive data type
+const exam = {
+    name : 'Rakibul Hasan',
+    subject: 'Programming and Problem Solving',
+    dept: "Computer Science And Engineering",
+    exams: {
+        exam_type : "Final Exam",
+        marks : 100
+    }
+}
+
+console.log(typeof exam);
+console.log(typeof name, typeof age);
