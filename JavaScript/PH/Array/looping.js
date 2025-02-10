ddee35f693b9bdf58729848214230baae3c3d699
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-02-11 01:17:52 +0600

    js

---
+++ JavaScript/PH/Array/looping.js
@@ -0,0 +1,18 @@
+// for loop
+const names = ["Rakib", "Hasan", "Ragib", "Arif", "Asib", "Maruf"];
+
+for(let i = 0;i<names.length;i++){
+    console.log(names[i]);
+}
+
+// while loop
+console.log()
+console.log()
+console.log()
+console.log("While Loop")
+
+let j = 0;
+while(j<names.length){
+    console.log(names[j]);
+    j++;
+}
