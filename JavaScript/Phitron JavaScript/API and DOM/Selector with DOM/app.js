dcbc96d17865f8d900014c79582d5f4c46ae61da
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-02-11 20:44:26 +0600

    js

---
+++ JavaScript/Phitron JavaScript/API and DOM/Selector with DOM/app.js
@@ -0,0 +1,7 @@
+// console.log(document.getElementsByTagName("h1"));
+// const target = document.getElementsByClassName('title');
+// console.log(target[0]);
+
+const target = document.getElementById("title");
+// console.log(target)
+target.style.color = "green";
