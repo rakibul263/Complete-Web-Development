2b7b121180d346df9a8cb9194f8814d263acc94a
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-02-11 18:33:47 +0600

    map  function

---
+++ JavaScript/Phitron JavaScript/map.js
@@ -0,0 +1,10 @@
+const products = [
+    {id:1, name: "Apple", model:"Air M3", price: 1200000, chip: "M3"},
+    {id:2, name: "HP", model:"Elite Book", price: 100000, chip: "AMD 5"},
+    {id:3, name: "Acer", model:"Asrion 3", price: 40000, chip: "Intel core i5"},
+    {id:4, name: "HP", model:"Daffodil Version", price: 23000, chip: "Core i3"},
+]
+
+const result = products.map(pd => pd.id*3);
+
+console.log(result);
