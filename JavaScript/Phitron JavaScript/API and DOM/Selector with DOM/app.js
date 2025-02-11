dcb2d24ce8b5a70543b22f51441c03c8bf420aec
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-02-11 23:39:25 +0600

    js

--- JavaScript/Phitron JavaScript/API and DOM/Selector with DOM/app.js
+++ JavaScript/Phitron JavaScript/API and DOM/Selector with DOM/app.js
@@ -5,3 +5,15 @@
 const target = document.getElementById("title");
 // console.log(target)
 target.style.color = "green";
+
+const allBox = document.getElementsByClassName("box");
+
+for(let i = 0; i<allBox.length; i++){
+    let element = allBox[i];
+
+    element.style.backgroundColor = "green";
+
+    if(element.innerText == 'Box-05'){
+        element.style.backgroundColor = 'blue';
+    }
+}
