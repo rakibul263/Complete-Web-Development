b97d6e5bb5ba6e952ed17102a92e4bc0dea67a1d
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-02-15 11:39:33 +0600

    all about the math function

---
+++ JavaScript/Programming Hero/Simple Problem/mathFunction.js
@@ -0,0 +1,23 @@
+// minimum value 
+console.log(Math.min(4,23,6,23,3,76,86,-3,543,75));
+
+// max value 
+console.log(Math.max(4,23,6,23,3,76,86,-3,543,75));
+
+// rounded value
+console.log(Math.round(3.44));
+console.log(Math.round(3.57));
+console.log(Math.round(3.50)); 
+
+
+// floor value
+console.log('Floor Value is : ',Math.floor(3.546));
+
+// ceiling value
+console.log(`Ceil value is : `, Math.ceil(3.43));
+
+// random number
+console.log('Random Number is up to 1 : ', Math.random().toFixed(4));
+
+//random number up to 5
+console.log('Random number up to 5 : ',Math.round(Math.random()*5));
