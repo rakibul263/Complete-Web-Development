ce8ca43d0576315429a552f67039a5fd76f80f4d
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-02-15 11:54:08 +0600

    date function

---
+++ JavaScript/Programming Hero/Simple Problem/date.js
@@ -0,0 +1,13 @@
+
+// some way to get the date
+const way01 = new Date();//today date 
+console.log(way01);
+
+const way02 = new Date("2021-10-01");
+console.log(way02);
+
+const way03 = new Date(2021,10);
+console.log(way03);
+
+const way04 = new Date(2020, 11, 23);
+console.log(way04);
