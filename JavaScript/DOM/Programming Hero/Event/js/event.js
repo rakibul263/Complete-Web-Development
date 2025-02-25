d46faeaf1281bb9d6a3001bd5872b408b64dcf19
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-02-25 22:50:44 +0600

    add event listener

--- JavaScript/DOM/Programming Hero/Event/js/event.js
+++ JavaScript/DOM/Programming Hero/Event/js/event.js
@@ -2,3 +2,15 @@ const makeB = document.getElementById('makeBlue');
 makeB.onclick = function makeBlue(){
     document.body.style.backgroundColor = 'blue';
 }
+
+document.getElementById('btn-make-green').addEventListener('click',
+    function makeGreen(){
+        // console.log(53453);
+        document.body.style.backgroundColor = 'green';
+    })
+
+document.getElementById('btn-make-gold').addEventListener('click',
+    function (){
+        document.body.style.backgroundColor = 'goldenrod'
+    }
+)
