f20dc5a039e8998a4d3038f4a86b455f6c427a21
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-03-12 13:06:30 +0600

    callback hell

--- JavaScript/Aditional/callback_hell.js
+++ JavaScript/Aditional/callback_hell.js
@@ -1,5 +1,5 @@
 const h1 = document.querySelector('h1');
-
+/*
 setTimeout(()=>{
     h1.style.color = 'red';
     console.log('Color changed successfully')
@@ -14,3 +14,15 @@ setTimeout(()=>{
     h1.style.color = 'orange';
     console.log('color change green to orange successfully');
 }, 3000)
+
+ */
+
+function colorChanged(color, delay){
+    setTimeout(()=>{
+        h1.style.color = color;
+    }, delay)
+}
+
+colorChanged('red', 1000);
+colorChanged('green', 2000);
+colorChanged('blue', 2000);
