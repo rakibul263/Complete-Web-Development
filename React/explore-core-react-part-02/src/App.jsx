46dd56800e4a4e1628981d86d7d5d298bc9db061
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-04-07 09:35:57 +0600

    react

--- React/explore-core-react-part-02/src/App.jsx
+++ React/explore-core-react-part-02/src/App.jsx
@@ -1,8 +1,13 @@
-import { useState } from 'react'
+import { Suspense, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Counter from './Counter'
+import Users from './users'
+// import { Suspense } from 'react'
+
+const fetchUsers = fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => res.json())
 
 function App() {
   const handelClick = ()=>{
@@ -20,6 +25,9 @@ function App() {
       <button onClick={handelClick}>Click Me</button><br />
       <button onClick={()=>handelAdd(6)}>Add Ten</button>
       <Counter></Counter>
+      <Suspense fallback={<h3>Loading...</h3>}>
+          <Users fetchUsers={fetchUsers}></Users>
+      </Suspense>
     </>
   )
 }
