28e6555e365e932b8a6cf3072ad99be24bdbf703
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-04-09 17:22:19 +0600

    react';

---
+++ React/ES6-import-export/src/utils/math/math.js
@@ -0,0 +1,7 @@
+const add = (a, b) => a + b;
+const diff = (a, b) => a - b;
+const multiplyTwoNumber = (a, b) => a * b;
+const divide = (a, b) => a/b;
+
+
+export {add, diff, multiplyTwoNumber as mult, divide}
